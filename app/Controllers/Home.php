e4b24eb4e19f28f530a3329a8eabf3add5dc87e2
amalliyahdefi-rgb <amalliyahdefi@gmail.com>
2025-08-27 08:54:11 +0700

    initial commit depi

---
+++ app/Controllers/Home.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('auth/login');
+    }
+
+    public function register(): string
+    {
+        return view('auth/register');
+    }
+
+    public function user(): string
+    {
+        return view('user/index');
+    }
+
+    public function dataIndikator(): string
+    {
+        return view('admin/data_indikator');
+    }
+}
---
+++ app/Views/admin/data_indikator.php
@@ -0,0 +1,3 @@
+<!-- app/Views/admin/data_indikator.php -->
+<h1>Halaman Data Indikator</h1>
+<p>Ini halaman untuk mengelola data indikator.</p>
